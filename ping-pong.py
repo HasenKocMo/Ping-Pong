31c3e47ea8287c0109ef6a6cb5b239f8b8f4f53b
HasenKocMo <82264452+HasenKocMo@users.noreply.github.com>
2021-04-17 13:31:22 +0300

    1.1 Beta
    Added platforms

--- ping-pong.py
+++ ping-pong.py
@@ -7,6 +7,29 @@ window = display.set_mode((win_width, win_height))
 display.set_caption('Ping-Pong')
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_RIGHT] and self.rect.x < 500:
+            self.rect.x += self.speed
+        if keys[K_LEFT] and self.rect.x > win_width - 80:
+            self.rect.x -= self.speed
+
+player1 = Player('platformaa.png', 5, win_height - 100, 80, 100, 10)
+player2 = Player('platformaa.png', 10, win_height - 80, 80, 100, 10)
+
 clock = time.Clock()
 FPS = 60
 game = True
@@ -15,6 +38,12 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    player1.update()
+    player2.update()
+
+    player1.reset()
+    player2.reset()
 
     display.update()
     clock.tick(FPS)
