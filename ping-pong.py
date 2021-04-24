280f494971a743ceab7f94e34ecbec817ee63ae2
HasenKocMo <82264452+HasenKocMo@users.noreply.github.com>
2021-04-24 12:48:30 +0300

    Beta 1.2
    Around a full version of Ping Pong!

--- ping-pong.py
+++ ping-pong.py
@@ -8,9 +8,9 @@ display.set_caption('Ping-Pong')
 window.fill(back)
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -20,30 +20,64 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y < 500:
-            self.rect.y += self.speed
-        if keys[K_DOWN] and self.rect.y > win_width - 80:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
-player1 = Player('racket.png', 5, win_height - 100, 80, 100, 10)
-player2 = Player('racket.png', 10, win_height - 80, 80, 100, 10)
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('BallP.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 20)
+lose1 = font.render('Player 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('Player 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 clock = time.Clock()
 FPS = 60
 game = True
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    player1.update()
-    player2.update()
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-    player1.reset()
-    player2.reset()
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y <0:
+            speed_y *= -1
+
+        if ball.rect.x <0: 
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
     display.update()
     clock.tick(FPS)
